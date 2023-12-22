043bea71b336f13703bbd42bd1f12073c7ef733e
SharmaTushar <sharmahimesh282@gmail.com>
2023-12-22 07:46:37 +0530

    added

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener('scroll', function() {
+  const leftSection = document.getElementById('leftSection');
+  leftSection.style.height = `${leftSection.offsetHeight + 10}px`;
+});
+
+  
